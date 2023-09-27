477596cbcd0997adcd0a8bf154517bfeafb88fbe
Tais Costa <taiscosta.eng@gmail.com>
2023-09-27 13:28:23 -0300

    nova rota criada: deletar conta

--- src/controladores/contas.js
+++ src/controladores/contas.js
@@ -1,7 +1,8 @@
-let { banco, contas, saques, depositos, transferencias } = require('../bancodedados');
+let { contas } = require('../bancodedados');
 
 const cadastroDeNovaContaBancaria = (req, res) => {
     const { nome, cpf, data_nascimento, telefone, email, senha } = req.body;
+
     if (!nome) {
         return res.status(400).json({ mensagem: 'O nome é obrigatório' })
     }
@@ -27,14 +28,15 @@ const cadastroDeNovaContaBancaria = (req, res) => {
     }
 
     let saldo = 0
-    let identificadorDeContaBancaria = 1
 
-    contas.find((numeroDaContaAtual) => {
-        return numeroDaContaAtual > numero
+    // pega todas as contas e busca o maior numero da conta e soma mais um
+
+    contas.find((maiorNumeroDeConta) => {
+        return maiorNumeroDeConta >= contas.numero
     })
 
     const novaContaBancaria = {
-        numero: identificadorDeContaBancaria++,
+        numero: maiorNumeroDeConta,
         saldo,
         usuario: {
             nome,
@@ -64,7 +66,77 @@ const listarContas = (req, res) => {
 }
 
 
+const atualizarUsuario = (req, res) => {
+    const { numeroConta, usuario } = req.params;
+    const { nome, cpf, data_nascimento, telefone, email, senha } = req.body;
+
+    if (!nome) {
+        return res.status(400).json({ mensagem: 'O nome é obrigatório' })
+    }
+
+    if (!cpf) {
+        return res.status(400).json({ mensagem: 'O cpf é obrigatório' })
+    }
+
+    if (!data_nascimento) {
+        return res.status(400).json({ mensagem: 'A data de nascimento é obrigatória' })
+    }
+
+    if (!telefone) {
+        return res.status(400).json({ mensagem: 'O telefone é obrigatório' })
+    }
+
+    if (!email) {
+        return res.status(400).json({ mensagem: 'O email é obrigatório' })
+    }
+
+    if (!senha) {
+        return res.status(400).json({ mensagem: 'A senha é obrigatória' })
+    }
+
+    const conta = contas.find((conta) => {
+        return conta.numeroConta === Number(numeroConta);
+    });
+
+    if (!conta) {
+        return res.status(404).json({
+            mensagem:
+                'Conta não encontrada'
+        });
+    }
+
+    conta.nome = nome;
+    conta.cpf = cpf;
+    conta.data_nascimento = data_nascimento;
+    conta.telefone = telefone;
+    conta.email = email;
+    conta.senha = senha;
+
+    return res.status(204).send();
+}
+
+const excluirConta = (req, res) => {
+    const { numeroConta } = req.params;
+
+    const conta = contas.find((conta) => {
+        return conta.numeroConta === Number(numeroConta);
+    });
+
+    if (!conta) {
+        return res.status(404).json({ mensagem: 'A conta não existe.' })
+    }
+
+    conta = contas.filter((conta) => {
+        return conta.numeroConta !== Number(numeroConta);
+    });
+
+    return res.status(204).send();
+}
+
+
 module.exports = {
     cadastroDeNovaContaBancaria,
-    listarContas
+    listarContas,
+    atualizarUsuario,
+    excluirConta
 }
--- src/rotas.js
+++ src/rotas.js
@@ -8,8 +8,13 @@ const rotas = express();
 rotas.post('/contas', contas.cadastroDeNovaContaBancaria);
 
 //listar contas através de liberação de senha
-rotas.get('/contas', contas.listarContas)
+rotas.get('/contas', contas.listarContas);
 
+//atualizar usuário da conta bancária
+rotas.put('/contas/:numeroConta/usuario', contas.atualizarUsuario);
+
+//deletar conta bancária
+rotas.delete('/contas/:numeroConta', contas.excluirConta);
 
 
 module.exports = rotas;
